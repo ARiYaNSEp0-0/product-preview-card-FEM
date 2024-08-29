248784f5a3afb4d0a9eee50a107b39dfcf673c53
AriyanSep0_0 <ariansepahi.v8@gmail.com>
2024-08-29 12:27:38 +0330

    :tada: added design system to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {
-
+      screen: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      },
+      fontFamily: {
+        sans: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Fraunces"', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
